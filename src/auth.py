154929149a5b261e54ace3ab73acef71a174923c
Roberto González <roberto.gonzalez@sparktechs.com>
2025-11-11 10:02:15 -0800

    Login required

--- src/auth.py
+++ src/auth.py
@@ -10,9 +10,9 @@ from flask import (
     session,
     url_for,
 )
-from typing import cast
+
+from typing import Any, Callable
 from werkzeug.security import check_password_hash, generate_password_hash
-from werkzeug import Response
 
 from src.db import get_db
 
@@ -71,3 +71,32 @@ def login():
         flash(error)
 
     return render_template("auth/login.html")
+
+
+@bp.before_app_request
+def load_logged_in_user():
+    user_id = session.get("user_id")
+
+    if user_id is None:
+        g.user = None
+    else:
+        g.user = (
+            get_db().execute("SELECT * FROM user WHERE id = ?", (user_id,)).fetchone()
+        )
+
+
+@bp.route("/logout")
+def logout():
+    session.clear()
+    return redirect(url_for("index"))
+
+
+def login_required(view: Callable[..., Any]) -> Callable[..., Any]:
+    @functools.wraps(view)
+    def wrapped_view(**kwargs: Any):
+        if g.user is None:
+            return redirect(url_for("auth.login"))
+
+        return view(**kwargs)
+
+    return wrapped_view
