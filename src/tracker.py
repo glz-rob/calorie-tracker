54e48e42398674b2759b187289fc5c58b60d2661
rob <rcgr.13@gmail.com>
2025-11-12 11:54:41 -0800

    Show calories per day

--- src/tracker.py
+++ src/tracker.py
@@ -11,14 +11,35 @@ bp = Blueprint("tracker", __name__)
 @login_required
 def index():
     db = get_db()
-    logs = db.execute(
-        "SELECT datetime, c.id, food, calories "
-        "FROM calorie_log c "
-        "JOIN date_log d ON c.date_id = d.id "
+    dates = db.execute(
+        "SELECT d.id, datetime "
+        "FROM date_log d "
         "JOIN user u ON d.user_id = u.id "
         "WHERE u.id == (?) "
         "ORDER BY datetime ASC ",
         (g.user["id"],),
     ).fetchall()
 
-    return render_template("tracker/index.html", logs=logs)
+    return render_template("tracker/index.html", dates=dates)
+
+
+@bp.route("/logs/<int:date_id>", methods=("GET",))
+def date_logs(date_id: int):
+    db = get_db()
+    date = db.execute(
+        "SELECT datetime FROM date_log WHERE id == (?)", (date_id,)
+    ).fetchone()
+    logs = db.execute(
+        "SELECT food, calories "
+        "FROM calorie_log c "
+        "JOIN date_log d ON c.date_id = d.id "
+        "JOIN user u ON d.user_id = u.id "
+        "WHERE u.id == (?) AND d.id == (?) "
+        "ORDER BY datetime ASC;",
+        (
+            g.user["id"],
+            date_id,
+        ),
+    ).fetchall()
+
+    return render_template("tracker/date_logs.html", date=date, logs=logs)
